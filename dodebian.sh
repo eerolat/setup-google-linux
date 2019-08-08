8ca7e78bf799c90b480b09e1a08c94fc70bbdc5a
eerolat <46994042+eerolat@users.noreply.github.com>
2019-08-08 12:00:24 +0300

    Add files via upload

---
+++ dodebian.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo '==== This script will enable Graphical User Interface on your Debian Server ===='
+echo '==== and creates a user account that you can use with Remote Desktop Connections ===='
+echo '==== Username: ' $1
+echo '==== Password: ' $2
+
+echo ''
+
+echo '==== Upgrade system ===='
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo '==== Install packages ===='
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gnome
+sudo apt-get install -y autocutsel
+sudo apt install -y xrdp
+sudo systemctl enable xrdp
+sudo apt-get install -y gnome-core
+sudo apt-get install -y gnome-panel
+sudo apt-get install -y synaptic
+
+sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
+
+touch ~/.Xresources
+
+sudo adduser  --disabled-password --gecos "GUI User" $1
+echo "$1:$2" | sudo chpasswd
+sudo /usr/sbin/usermod -aG sudo $1
---
+++ doubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo '==== This script will enable Graphical User Interface on your Ubuntu Server ===='
+echo '==== and creates a user account that you can use with Remote Desktop Connections ===='
+echo '==== Username: ' $1
+echo '==== Password: ' $2
+
+echo ''
+
+echo '==== Upgrade system ===='
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo '==== Install packages ===='
+sudo apt-get install -y gnome-shell
+sudo apt-get install -y ubuntu-gnome-desktop
+sudo apt-get install -y autocutsel
+sudo apt install -y xrdp
+sudo systemctl enable xrdp
+sudo apt-get install -y gnome-core
+sudo apt-get install -y gnome-panel
+sudo apt-get install -y synaptic
+
+touch ~/.Xresources
+
+sudo adduser  --disabled-password --gecos "GUI User" $1
+echo "$1:$2" | sudo chpasswd
+sudo /usr/sbin/usermod -aG sudo $1
